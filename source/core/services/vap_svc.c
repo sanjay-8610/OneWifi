2f69144b2e8279e8ec5ab5cfc3223eba3dc83f4e
Raja Shah <167130396+rshah-1@users.noreply.github.com>
2025-12-10 13:46:45 -0800

    Merge pull request #745 from navyasher/RDKB-62695_Onewifi_coverity
    Rdkb 62695: Fix Onewifi high priority Coverity issues

--- source/core/services/vap_svc.c
+++ source/core/services/vap_svc.c
@@ -189,7 +189,7 @@ int update_lnf_psk_vap_hal_prop_bridge_name(vap_svc_t *svc, wifi_vap_info_map_t 
             } else {
                 if (strncmp(if_prop->bridge_name, vap_map->vap_array[j].bridge_name, strlen(vap_map->vap_array[j].bridge_name)) != 0) {
                     wifi_util_info_print(WIFI_CTRL,"%s:%d: changed bridge name from :%s to %s\n", __func__, __LINE__, if_prop->bridge_name, vap_map->vap_array[j].bridge_name);
-                    strncpy(if_prop->bridge_name, vap_map->vap_array[j].bridge_name, sizeof(vap_map->vap_array[j].bridge_name));
+                    snprintf(if_prop->bridge_name, sizeof(if_prop->bridge_name), "%s", vap_map->vap_array[j].bridge_name);
                 }
             }
         }
